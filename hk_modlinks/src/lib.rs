6c88188d5ec12ff5d92cac2454fb5598d0a81ceb
Frank Yang <clazex.y@gmail.com>
2023-12-28 22:06:22 +0800

    refactor: move safe name to root

--- hk_modlinks/src/lib.rs
+++ hk_modlinks/src/lib.rs
@@ -9,9 +9,6 @@ mod platform;
 mod tag;
 mod version;
 
-#[cfg(feature = "safe-name")]
-mod safe_name;
-
 #[cfg(feature = "xml")]
 mod xml;
 
@@ -33,9 +30,6 @@ pub use platform::*;
 pub use tag::*;
 pub use version::*;
 
-#[cfg(feature = "safe-name")]
-pub use safe_name::*;
-
 #[cfg(feature = "xml")]
 pub use xml::{ApiLinks as ApiLinksXmlCompat, ModLinks as ModLinksXmlCompat};
 
@@ -47,3 +41,35 @@ pub use mod_links_changelog::*;
 
 #[cfg(feature = "changelog-template")]
 pub use changelog_template::*;
+
+/// Find a suitable counterpart for mod name that behaves well when
+/// using as filesystem path, URL path and part of XML. Does not promise
+/// absolute uniqueness, but should be sufficiently unique.
+///
+/// Steps in detail:
+/// 1. Split the string into parts by characters: `*`, `?`, `:`, `<`, `>`, `"`,
+/// `/`, `|`, `\`, `\t` (tab), ` ` (space), `[` and `]`, empty parts are ignored
+/// throughout the process;
+/// 2. Remove all characters that are neither ascii-alphanumeric nor `.`, `-`, `_`;
+/// 4. Make first character in each part uppercase;
+/// 5. Join the parts together.
+#[must_use]
+pub fn get_safe_mod_name(name: &str) -> String {
+    name.split([
+        ' ', // Not good for URI
+        '[', ']', // Not good for XML CDATA
+    ])
+    .filter(|s| !s.is_empty())
+    .map(|d| {
+        d.replace(
+            |c: char| !matches!(c, '.' | '-' | '_') && !c.is_ascii_alphanumeric(),
+            "",
+        )
+    })
+    .filter(|s| !s.is_empty())
+    .map(|mut s| {
+        s[0..1].make_ascii_uppercase();
+        s
+    })
+    .collect::<String>()
+}
